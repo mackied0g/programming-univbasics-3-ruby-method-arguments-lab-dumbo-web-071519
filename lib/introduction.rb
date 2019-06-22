caa47ab37008cd5e08ec0f7fdcfbe89a5a25bdc0
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-06-22 00:04:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction(name) 
-puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 introduction("Mackenzie")
 
 def introduction_with_language (name, language)
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
